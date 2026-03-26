684a9dd5c58dcdea46b1227cd7e693a0eecb6e03
=Noorulain2004 <=noorii4906@gmail.com>
2026-03-26 22:43:17 +0500

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
-import { Routes, Route, Outlet } from "react-router-dom"; // 1. Outlet import karein
+import { Routes, Route, Outlet } from "react-router-dom";
 import Navbar from "./Components/Navbar/Navbar.jsx";
 import ScrollToTop from './Components/ScrollToTop/ScrollToTop.jsx';
 import Home from "./Pages/Home.jsx";
 import Working from "./Pages/working.jsx";
 import Model from "./Pages/Model.jsx";
 import FAQ from "./Pages/FAQ.jsx";
-import Contact from "./Pages/Contact.jsx"; // Check karein agar filename ContactUs.jsx hai to wohi likhein
+import Contact from "./Pages/Contact.jsx"; 
 import Agreement from "./Components/Agreement/Agreement.jsx";
 import "./App.css";
 
-// 2. Ek Layout Component banayen jis main Navbar ho
+
 const LayoutWithNavbar = () => {
   return (
     <>
       <ScrollToTop />
       <Navbar />
-      <Outlet /> {/* Baqi saray pages yahan render hon gay */}
+      <Outlet /> 
     </>
   );
 };
@@ -23,11 +23,9 @@ const LayoutWithNavbar = () => {
 function App() {
   return (
     <>
-      {/* <Navbar />  <-- Yahan sy global Navbar hata den */}
+      
 
       <Routes>
-        
-        {/* 3. Woh pages jin pr Navbar dikhana hai, unhen is Layout main rakhen */}
         <Route element={<LayoutWithNavbar />}>
           <Route path="/" element={<Home />} />
           <Route path="/how-it-works" element={<Working />} />
@@ -35,8 +33,6 @@ function App() {
           <Route path="/faq" element={<FAQ />} />
           <Route path="/contact" element={<Contact />} />
         </Route>
-
-        {/* 4. Agreement page ko bahar rakhen (No Navbar here) */}
         <Route path="/agreement" element={<Agreement />} />
         
       </Routes>
